30e68de2aa974c3fcbd28e361bac53637f32187d
enoch232 <enoch232@gmail.com>
2016-08-09 23:26:43 -0700

    add a test

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 
 describe Article do
   it 'has a valid factory' do
-  	expect(FactoryGirl.build(:article, title: "title")).to be_valid
+  	user = double(:user, user_id: 1)
+  	expect(FactoryGirl.build(:article, title: "title", user_id: user.id)).to be_valid
   end
   it 'validates the presence of a title' do
   	expect(FactoryGirl.build(:article)).to be_invalid
